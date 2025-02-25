70fe81d5bf890173392190e52061f4d1af63f202
Greg Liaskas <greg.liaskas@outlook.com>
2025-02-25 20:34:17 +0200

    refactor: ScopeHelper.DummyInvocationScope() -> ScopeHelper.DummyEscapedLink()

--- _devHelp/src/ScopeHelper.cs
+++ _devHelp/src/ScopeHelper.cs
@@ -4,7 +4,7 @@ namespace _devHelp;
 
 public static class ScopeHelper
 {
-    public static LinkTo<T> DummyInvocationScope<T>(Func<T> createT) where T : struct
+    public static LinkTo<T> DummyEscapedLink<T>(Func<T> createT) where T : struct
     {
         var scopedContext = createT();
         return LinkTo<T>.Create(in scopedContext);
