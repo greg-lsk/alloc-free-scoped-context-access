8d016f172c431e3fd9cb4877f91de5942b75c4be
Greg Liaskas <greg.liaskas@outlook.com>
2025-02-25 15:57:48 +0200

    create: LinkTo.cs

--- src/obj/Debug/net9.0/src.AssemblyInfo.cs
+++ src/obj/Debug/net9.0/src.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("src")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9430ab4901af01a8047a1019595debb6a8819b0")]
 [assembly: System.Reflection.AssemblyProductAttribute("src")]
 [assembly: System.Reflection.AssemblyTitleAttribute("src")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
