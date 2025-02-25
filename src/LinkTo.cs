baf1e66f4945f139158a52bed9518d67ea0de7ee
Greg Liaskas <greg.liaskas@outlook.com>
2025-02-25 16:25:41 +0200

    new: craeted  LinkTo<> type

---
+++ src/LinkTo.cs
@@ -0,0 +1,20 @@
+namespace src;
+
+
+public unsafe readonly struct LinkTo<TTarget> where TTarget : struct
+{
+    private readonly TTarget* _target;
+    private LinkTo(TTarget* target) => _target = target;
+
+    public ref TTarget Target => ref *_target;
+
+    internal static LinkTo<TTarget> Create(in TTarget _target)
+    {
+        fixed (TTarget* ptr = &_target)
+        {
+            return new LinkTo<TTarget>(ptr);
+        }
+    }
+
+    public bool IsVoid() => _target is null;
+}
