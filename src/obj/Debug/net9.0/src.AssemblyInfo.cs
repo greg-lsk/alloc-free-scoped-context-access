e463797d7ecab08443231a5fdbaf8791efda6f8e
Greg Liaskas <greg.liaskas@outlook.com>
2025-02-25 16:29:25 +0200

    new: IRemoteFunc<,>, IRemote<>, IRemoteAccessor<> interfaces

--- src/obj/Debug/net9.0/src.AssemblyInfo.cs
+++ src/obj/Debug/net9.0/src.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("src")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9430ab4901af01a8047a1019595debb6a8819b0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+baf1e66f4945f139158a52bed9518d67ea0de7ee")]
 [assembly: System.Reflection.AssemblyProductAttribute("src")]
 [assembly: System.Reflection.AssemblyTitleAttribute("src")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
