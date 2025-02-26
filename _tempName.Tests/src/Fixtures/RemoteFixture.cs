c90990b3e868759c20e373efabacc5377c940d8d
Greg Liaskas <greg.liaskas@outlook.com>
2025-02-26 23:01:01 +0200

    refactor: enhance RemoteFixture with LinkState enum and ArrangeLink delegate for improved link handling

--- _tempName.Tests/src/Fixtures/RemoteFixture.cs
+++ _tempName.Tests/src/Fixtures/RemoteFixture.cs
@@ -2,12 +2,26 @@ using _devHelp;
 
 namespace _tempName.Tests.Fixtures;
 
+public enum LinkState
+{
+    Active,
+    Inactive
+}
+
+public delegate LinkTo<T> ArrangeLinkTo<T>(LinkState link, in T dummy = default) where T : struct;
 
 public class RemoteFixture : IDisposable
 {
     public IRemote<ReadonlyDummyStruct> Target;
+    public static  ArrangeLinkTo<ReadonlyDummyStruct> ArrangeLink => ArrangeLinkTo;
 
     public RemoteFixture() => Target = new DummyRemote();
 
     public void Dispose() => Target = null;
+
+    private static LinkTo<T> ArrangeLinkTo<T>(LinkState link, in T dummy) where T : struct => link switch
+    {
+        LinkState.Active => LinkTo<T>.Create(in dummy),
+        LinkState.Inactive => default,
+    };
 }
--- _tempName.Tests/src/IRemoteTests.cs
+++ _tempName.Tests/src/IRemoteTests.cs
@@ -1,36 +1,39 @@
 using _devHelp;
 using _tempName.Exceptions;
+using _tempName.Tests.Fixtures;
 
 namespace _tempName.Tests;
 
 
-public class IRemoteTests
+public class IRemoteTests(RemoteFixture remoteFixture) : IClassFixture<RemoteFixture>
 {
+    private readonly IRemote<ReadonlyDummyStruct> _remote = remoteFixture.Target;
+    private readonly ArrangeLinkTo<ReadonlyDummyStruct> _arrangeLink = RemoteFixture.ArrangeLink;
+
     [Fact]
     public void Get_ReturnsCorrectValue_WhenActiveLink_IsProvided()
     {
         // Arrange
-        IRemote<ReadonlyDummyStruct> dummyRemote = new DummyRemote();
-        var linkTo = LinkTo<ReadonlyDummyStruct>.Create(new(42, "Hellow"));
+        var dummy = new ReadonlyDummyStruct(1, "test");
+        var linkTo = _arrangeLink(LinkState.Active, in dummy);
 
         // Act
-        var intValue = dummyRemote.Get<DummyGetInt, int>(linkTo);
-        var stringValue = dummyRemote.Get<DummyGetString, string>(linkTo);
-
+        var intActual = _remote.Get<DummyGetInt, int>(linkTo);
+        var stringActual = _remote.Get<DummyGetString, string>(linkTo);
+        
         // Assert
-        Assert.Equal(expected: 42, actual: intValue);
-        Assert.Equal(expected: "Hellow", actual: stringValue);
+        Assert.Equal(expected: 1, actual: intActual);
+        Assert.Equal(expected: "test", actual: stringActual);
     }
 
     [Fact]
     public void Get_ThrowsInactiveLinkException_WhenInactiveLink_IsProvided()
     {
         // Arrange
-        IRemote<ReadonlyDummyStruct> dummyRemote = new DummyRemote();
-        LinkTo<ReadonlyDummyStruct> link = default;
+        var linkTo = _arrangeLink(LinkState.Inactive);
 
         // Act
-        void act() => dummyRemote.Get<DummyGetInt, int>(link);
+        void act() => _remote.Get<DummyGetInt, int>(linkTo);
 
         // Assert
         var exception = Assert.Throws<InactiveLinkException>(act);
