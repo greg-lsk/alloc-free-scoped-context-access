3273dec796275118eec87539c1a416a0567f0443
Greg Liaskas <greg.liaskas@outlook.com>
2025-02-26 15:59:26 +0200

    refactor: deleated ImmutableDummyStruct, since it was unused

--- _devHelp/src/DummyStruct.cs
+++ _devHelp/src/DummyStruct.cs
@@ -6,9 +6,3 @@ public readonly struct ReadonlyDummyStruct(int intValue, string stringValue)
     public int IntValue { get; } = intValue;
     public string StringValue { get; } = stringValue;
 }
-
-public struct MutableDummyStruct(int intValue, string stringValue)
-{
-    public int IntValue { get; set; } = intValue;
-    public string StringValue { get; set; } = stringValue;
-}
