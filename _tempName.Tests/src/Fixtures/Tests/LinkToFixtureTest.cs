cd9c809dd9960f08ddfa24da4ecf6d2be9fecfcd
Greg Liaskas <greg.liaskas@outlook.com>
2025-02-27 02:54:36 +0200

    test: add LinkToFixtureTest to validate ArrangeLinkTo functionality and error handling

---
+++ _tempName.Tests/src/Fixtures/Tests/LinkToFixtureTest.cs
@@ -0,0 +1,61 @@
+namespace _tempName.Tests.Fixtures.Tests;
+
+public class LinkToFixtureTest
+{
+    [Fact]
+    public void ArrangeLinkTo_Returns_CorrectLink_WhenPrompted_ToReturnAnActiveOne()
+    {
+        // Arrange
+        var fixture = new LinkToFixture();
+
+        // Act
+        var linkFromProvidedTarget = fixture.ArrangeLinkTo(LinkState.Active, 1);
+        var actualTarget = linkFromProvidedTarget.Target;
+
+        // Assert
+        Assert.Equal(actual: actualTarget, expected: 1);
+    }    
+
+    [Fact]
+    public void ArrangeLinkTo_Returns_ActiveLink_WhenPrompted_RegardlessOf_HavingTargetProvided()
+    {
+        // Arrange
+        var fixture = new LinkToFixture();
+
+        // Act
+        var linkFromProvidedTarget = fixture.ArrangeLinkTo(LinkState.Active, 1);
+        var linkFromNonProvidedTarget = fixture.ArrangeLinkTo<int>(LinkState.Active);
+
+        // Assert
+        Assert.True(linkFromProvidedTarget.IsActive());
+        Assert.True(linkFromNonProvidedTarget.IsActive());
+    }
+
+    [Fact]
+    public void ArrangeLinkTo_Returns_InactiveLink_WhenPrompted_RegardlessOf_HavingTargetProvided()
+    {
+        // Arrange
+        var fixture = new LinkToFixture();
+
+        // Act
+        var linkFromProvidedTarget = fixture.ArrangeLinkTo(LinkState.Inactive, 1);
+        var linkFromNonProvidedTarget = fixture.ArrangeLinkTo<int>(LinkState.Inactive);
+
+        // Assert
+        Assert.False(linkFromProvidedTarget.IsActive());
+        Assert.False(linkFromNonProvidedTarget.IsActive());
+    }
+
+    [Fact]
+    public void ArrangeLinkTo_Throws_ArgumentException_WhenProvidingInvalidState()
+    {
+        // Arrange
+        var fixture = new LinkToFixture();
+
+        // Act
+        void act() => fixture.ArrangeLinkTo((LinkState)8, 2);
+
+        // Assert
+        Assert.Throws<ArgumentException>(act);
+    }         
+}
