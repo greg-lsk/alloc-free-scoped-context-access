1493ab89c9e33641ce24a1e2d54d1852fd28df0d
Greg Liaskas <greg.liaskas@outlook.com>
2025-02-27 02:19:18 +0200

    refactor: minor

--- _tempName.Tests/src/Fixtures/LinkToFixture.cs
+++ _tempName.Tests/src/Fixtures/LinkToFixture.cs
@@ -7,15 +7,15 @@ public enum LinkState
     Inactive
 }
 
-public delegate LinkTo<T> ArrangeLinkTo<T>(LinkState link, in T dummy = default) where T : struct;
+public delegate LinkTo<T> ArrangeLinkTo<T>(LinkState link, in T target = default) where T : struct;
 
 public class LinkToFixture : IDisposable
 {
     public void Dispose(){}
 
-    public LinkTo<T> ArrangeLinkTo<T>(LinkState link, in T dummy = default) where T : struct => link switch
+    public LinkTo<T> ArrangeLinkTo<T>(LinkState link, in T target = default) where T : struct => link switch
     {
-        LinkState.Active => LinkTo<T>.Create(in dummy),
+        LinkState.Active => LinkTo<T>.Create(in target),
         LinkState.Inactive => default,
     };  
 }
